89305c7799dba29ea830a9fcaf62239fa6d184ee
Akas679 <foodzone.edu.in@gmail.com>
2025-06-25 15:18:28 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -71,11 +71,11 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
  const PORT = process.env.PORT || 5050;
     app.listen(PORT, '0.0.0.0', () => {
-      console.log(✅ Server is running on http://0.0.0.0:${PORT});
+      console.log( Server is running on http://0.0.0.0:${PORT});
     });
 
   } catch (error) {
-    console.error("❌ Failed to start server:", error);
+    console.error(" Failed to start server:", error);
     process.exit(1);
   }
 })();
