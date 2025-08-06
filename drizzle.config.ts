8df4983097abe537e095a67c15b4d2b6ddf43380
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2025-08-06 18:42:45 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,7 @@ if (!databaseUrl) {
 }
 
 // Read SSL Certificate
-const sslCert = fs.readFileSync(path.resolve(__dirname, 'supabase-root-ca.crt.cer')).toString();
+const sslCert = fs.readFileSync(path.resolve(__dirname, './cert/supabase-root-ca.crt.cer')).toString();
 
 export default defineConfig({
   out: "./migrations",
