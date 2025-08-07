ffade1c37af561feec052b33cfffb8889538af2c
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2025-08-07 15:06:24 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL not set.");
 }
 
 export default defineConfig({
-  out: "./migrations",
   schema: "./shared/schema.ts",
+  out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: databaseUrl,
+    ssl: {
+      rejectUnauthorized: true, // Use trusted root CAs
+    },
   },
 });
